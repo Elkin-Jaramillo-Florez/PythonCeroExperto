29523eceeae100cac20559f45c2f25c4a56b03c4
Elkin Jaramillo Florez <elkin.jaramillo@dataknow.co>
2024-04-24 15:51:01 -0500

    variables

---
+++ FundamentosPython/02_variables.py
@@ -0,0 +1,15 @@
+# Esto son las variables
+my_name = "Elkin"
+print(my_name)
+# Esto es una variable de tipo int
+my_age = 26
+print(my_age)
+
+my_name = "David"
+
+print("Hola", my_name)
+
+# input
+my_name = input("¿Cuál es tu nombre?\n")
+my_age = input("Dime tu edad\n")
+print("Hola", my_name, "tu edad es", my_age, "años")
