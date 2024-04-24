729bd0aba3dd0b17aa2774d65b0ef48cf8122b89
Elkin Jaramillo Florez <elkin.jaramillo@dataknow.co>
2024-04-24 15:40:07 -0500

    print

---
+++ FundamentosPython/01_print.py
@@ -0,0 +1,18 @@
+print("Hola este es el archivo 01")
+
+print("Hola mundo desde python")
+
+print("Hola soy Elkin Jaramillo y tengo 26 años")
+
+print(8 + 10)
+print(10 - 5)
+print(5 * 4)
+print(9 / 3)
+
+# Este es un comentario de una sola linea
+
+""" 
+Este es un comentario
+de 
+varias líneas
+"""
