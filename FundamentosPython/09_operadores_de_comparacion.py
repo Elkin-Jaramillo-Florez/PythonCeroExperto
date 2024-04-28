520d570eebfc5228577313e2604d607a4d6eeed7
Elkin Jaramillo Florez <elkin.jaramillo@dataknow.co>
2024-04-28 12:21:32 -0500

    operadores de comparacion

---
+++ FundamentosPython/09_operadores_de_comparacion.py
@@ -0,0 +1,22 @@
+# > mayor que
+print(5 > 3)
+# < menor que
+print(5 < 3)
+# <= menor o igual
+print(5 <= 5)
+# >= mayor o igual
+print(10 >= 2)
+# == igual
+print(6 == 6)
+print(5 == 3)
+# != diferente
+print(6 != 6)
+print(5 != 3)
+# Comparación con strings
+print("Apple" == "Apple")
+print("Apple" == "apple")
+
+print("1" == 1)
+
+age = 15
+print(age >= 18)
