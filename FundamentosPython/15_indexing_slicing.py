4f9f9e61f52de8ecf11e306c5e45145a17cc4502
Elkin Jaramillo Florez <elkin.jaramillo@dataknow.co>
2024-04-28 18:11:20 -0500

    indexing slicing

---
+++ FundamentosPython/15_indexing_slicing.py
@@ -0,0 +1,15 @@
+text = "Ella sabe Python"
+print(text[0])
+print(text[1])
+print(text[-1])
+print(text[len(text) - 1])
+
+# slicing
+print(text[0:5])
+print(text[-6:])
+print(text[0:10])
+print(text[:10])
+print(text[:2])
+
+print(text[10:16:2])
+print(text[::2])
