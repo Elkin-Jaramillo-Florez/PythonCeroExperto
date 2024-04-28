df0b1a0016f8f09bd6c50f8b83a448e8284ab64c
Elkin Jaramillo Florez <elkin.jaramillo@dataknow.co>
2024-04-28 11:51:34 -0500

    transformaciones de tipos

---
+++ FundamentosPython/07_transformaciones_de_tipo.py
@@ -0,0 +1,23 @@
+name = "Elkin"
+print(type(name))
+
+name = 12
+print(type(name))
+
+name = True
+print(type(name))
+
+print("Elkin" + " Jaramillo")
+print(43 + 34)
+
+print("ELkin" + "12")
+
+age = 26
+print("Mi edad es " + str(age))
+
+print(f"Mi edad es {age}")
+
+age = input("Escribe tu edad ")
+print(type(age))
+age = int(age)
+print(f"Tu edad es 10 años sera {age + 10} años")
