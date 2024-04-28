b78aac99580eb5527f60a4ffa9d62c65b114a91f
Elkin Jaramillo Florez <elkin.jaramillo@dataknow.co>
2024-04-28 12:07:02 -0500

    operadores aritmeticos

---
+++ FundamentosPython/08_operadores_aritmeticos.py
@@ -0,0 +1,25 @@
+# Este es el orden de ejecución de operaciones aritméticas (Izquierda -> Derecha)
+""" 
+P - paréntesis
+E - exponentes
+M - multiplicación
+D - división
+A - adición
+S - sustracción
+ """
+
+print(10 + 3)
+print(10 - 3)
+print(10 * 3)
+print(10 / 3)
+print(10 // 3)
+print(10 % 2)
+print(10 % 3)
+print(2**3)
+
+print(2**3 + 3 - 7 / 1 // 4)
+
+
+print("Hola" + " Mundo")
+
+print("Hola" * 3)
