cc374be997aeab2f42a05a788888e94ed1f146f3
Elkin Jaramillo Florez <elkin.jaramillo@dataknow.co>
2024-04-28 17:53:14 -0500

    metodos para strings

---
+++ FundamentosPython/14_string_recargado.py
@@ -0,0 +1,27 @@
+text = "Ella sabe programar en Python"
+""" print("JavaScript" in text)
+print("Python" in text)
+
+if "JS" in text:
+    print("Elegiste bien")
+else:
+    print("También elegiste bien") """
+
+size = len(text)
+print(size)
+
+print(text)
+print(text.upper())
+print(text.lower())
+print(text.count("a"))
+print(text.swapcase())
+print(text.startswith("E"))
+print(text.endswith("n"))
+print(text.replace("Python", "Go"))
+
+text2 = "este es un titulo"
+print(text2)
+print(text2.capitalize())
+print(text2.title())
+print(text2.isdigit())
+print("232".isdigit())
