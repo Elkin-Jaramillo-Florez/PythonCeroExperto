afe9507bd773686a2eb38ad59064d059d2d9bf22
Elkin Jaramillo Florez <elkin.jaramillo@dataknow.co>
2024-04-28 18:22:18 -0500

    listas

---
+++ FundamentosPython/16_listas.py
@@ -0,0 +1,16 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(numbers)
+print(type(numbers))
+
+tasks = ["make a dishes", "play videogames"]
+print(tasks)
+
+types = [1, True, "Hola"]
+print(types[0])
+print(types[-1])
+types[-1] = "Python"
+print(types)
+
+print(numbers[:3])
+print(11 in numbers)
+print("Python" in types)
