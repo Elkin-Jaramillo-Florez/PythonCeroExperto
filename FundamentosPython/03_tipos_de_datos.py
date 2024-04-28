3f466b963296029417bca87bc841f93bd16c2002
Elkin Jaramillo Florez <elkin.jaramillo@dataknow.co>
2024-04-28 10:31:42 -0500

    tipos de datos

---
+++ FundamentosPython/03_tipos_de_datos.py
@@ -0,0 +1,16 @@
+# String
+my_name = "Elkin"
+print(type(my_name))
+# int
+my_age = 26
+print(type(my_age))
+# bool
+is_single = True
+print(type(is_single))
+
+
+my_age = input("¿Cuál es tu edad?\n")
+print(my_age)
+print(type(my_age))
+
+print("Hola", my_name, "tu edad es", my_age, "años")
