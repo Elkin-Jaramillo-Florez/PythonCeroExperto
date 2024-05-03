99d2714de4eb00f1a3440e7d97c1b1dd549918b0
Elkin Jaramillo Florez <elkin.jaramillo@dataknow.co>
2024-05-03 14:15:47 -0500

    funciones

---
+++ ComprehensionsFuncionesManejoErrores/07_funciones.py
@@ -0,0 +1,17 @@
+print("Hola")
+
+
+def my_print(text):
+    print(text * 2)
+
+
+# my_print('este es my texto')
+# my_print('Hola')
+
+
+def suma(a, b):
+    print(int(a) + int(b))
+
+
+suma(4, 2)
+suma(10, 5)
