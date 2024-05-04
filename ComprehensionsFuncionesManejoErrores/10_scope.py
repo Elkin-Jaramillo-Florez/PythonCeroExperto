5c8e5cddf4bed4798b12c439ccd0256dcc7e9a67
Elkin Jaramillo Florez <elkin.jaramillo@dataknow.co>
2024-05-04 08:30:01 -0500

    scope variables

---
+++ ComprehensionsFuncionesManejoErrores/10_scope.py
@@ -0,0 +1,16 @@
+price = 100  # global
+result = 200
+
+
+def increment():
+    price = 200
+    result = price + 10
+    print(result)
+    return result
+
+
+print(price)
+
+price_2 = increment()
+print(price_2)
+print(result)
