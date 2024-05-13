45e8cca21e296970607edabfe1e6bb699116f24e
Elkin-Jaramillo-Florez <167355527+Elkin-Jaramillo-Florez@users.noreply.github.com>
2024-05-13 16:37:20 +0000

    update repo

--- ComprehensionsFuncionesManejoErrores/app/charts.py
+++ ComprehensionsFuncionesManejoErrores/app/charts.py
@@ -9,11 +9,13 @@ def generate_bar_chart(labels, values):
   ax.bar(labels, values)
   plt.show()
 
+
 def generate_pie_chart(labels, values):
   fig, ax = plt.subplots()
   ax.pie(values, labels=labels)
   ax.axis('equal')
   plt.show()
+
 
 if __name__ == "__main__":
   # generate_bar_chart(labels, values)
