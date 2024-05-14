a3a2dc6a4f26c90dfa06628adf649006c0041f58
Elkin-Jaramillo-Florez <167355527+Elkin-Jaramillo-Florez@users.noreply.github.com>
2024-05-14 13:05:23 +0000

    solucion reto grafica poblacion

---
+++ ComprehensionsFuncionesManejoErrores/app/reto_population.py
@@ -0,0 +1,28 @@
+import csv
+import charts
+
+
+def run():
+  labels, values = read_population('./data.csv')
+  charts.generate_pie_chart(labels, values)
+
+
+def read_population(path):
+  with open(path, 'r') as file:
+    reader = csv.reader(file, delimiter=',')
+    header = next(reader)
+    country_population = {}
+    territory_index = header.index('Country/Territory')
+    population = header.index('World Population Percentage')
+    labels = []
+    values = []
+    for row in reader:
+      country_population[row[territory_index]] = row[population]
+    for key, value in country_population.items():
+      labels.append(key)
+      values.append(float(value))
+  return labels, values
+
+
+if __name__ == '__main__':
+  run()
